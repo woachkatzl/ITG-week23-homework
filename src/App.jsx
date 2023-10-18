de90bd395994a4a6f1f61998334862e5bb8f295b
woachkatzl <y.v.polonskaya@gmail.com>
2023-10-18 17:23:56 -0400

    populated html content

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { planData } from './domain/plan-data/data';
+import { PlanCard } from './presentation/plan-card';
 
 //Стили
 import styles from './assets/styles/global.module.scss';
 
-function App() {}
+function App() {
+  return (
+    <React.Fragment>
+      {planData.map((plan, index) => <PlanCard
+        key={index}
+        name={plan.name}
+        price={plan.price}
+        speed={plan.speed}
+      />)}
+    </React.Fragment>
+  );
+}
 
 export { App };
--- src/presentation/plan-card/index.jsx
+++ src/presentation/plan-card/index.jsx
@@ -1,2 +1,27 @@
 import React from 'react';
 import styles from './planCard.module.scss';
+
+function PlanCard(props) {
+  return (
+    <div className="card">
+      <div className="card__footer">
+        <h2 className="card__title">{props.name}</h2>
+      </div>
+      <div className="card__body">
+        <div className="card__price">
+          <p className="card__price--small">руб</p>
+          <p className="card__price">{props.price}</p>
+          <p className="card__price--small">/мес</p>
+        </div>
+        <div className="card__speed">
+          <p className="card__text">до {props.speed} Мбит/сек</p>
+        </div>
+      </div>
+      <div className="card__footer">
+        <p className="card__text">Объем включенного трафика не ограничен</p>
+      </div>
+    </div>
+  );
+}
+
+export { PlanCard };
